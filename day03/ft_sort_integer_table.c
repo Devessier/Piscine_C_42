b311d324c9dda20c966c88f54ad79b74767e158e
Devessier <baptiste@devessier.engineer>
2018-08-04 12:45:50 +0200

    Finished ex09 of day03

--- day03/ft_sort_integer_table.c
+++ day03/ft_sort_integer_table.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
-
 void	ft_sort_integer_table(int *tab, int size)
 {
 	int i;
@@ -21,20 +19,12 @@ void	ft_sort_integer_table(int *tab, int size)
 	tmp = 0;
 	while (++i < size)
 	{
-		if (i && i[tab] < tab[i - 1])
+		if (i[tab] < tab[i - 1])
 		{
 			tmp = i[tab];
 			tab[i] = tab[i - 1];
 			tab[i - 1] = tmp;
+			i = 0;
 		}
-	}
-
-	// boucle de print
-	i = -1;
-	while (++i < size)
-	{
-		ft_putchar(tab[i]);
-		ft_putchar(',');
-		ft_putchar(' ');
 	}
 }
