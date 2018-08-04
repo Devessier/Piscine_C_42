8a601b0ae59734f43b38b8fb11f1b260d1ed1e32
Devessier <baptiste@devessier.fr>
2018-08-04 18:37:56 +0200

    My two first exercises for Rush00

---
+++ Rush00/ex00/rush01.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdevessi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/04 14:59:36 by bdevessi          #+#    #+#             */
+/*   Updated: 2018/08/04 17:10:07 by bdevessi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char a);
+
+void	rush(int x, int y)
+{
+	int col;
+	int row;
+
+	row = -1;
+	if (x <= 0 || y <= 0)
+		return ;
+	while (++row < y)
+	{
+		col = -1;
+		while (++col < x)
+		{
+			if (!row || row == y - 1)
+			{
+				if (!col || col == x - 1)
+					ft_putchar(((!col && !row) || (col && row)) ? '/' : '\\');
+				else
+					ft_putchar('*');
+			}
+			else
+				ft_putchar((!col || col == x - 1) ? '*' : ' ');
+		}
+		ft_putchar('\n');
+	}
+}
